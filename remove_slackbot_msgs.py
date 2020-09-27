b4cfc94304f297bfc43510570729f7fe672ff66b
drw <drw@users.noreply.github.com>
2020-09-27 08:59:34 -0400

    Improve formatting of console output

--- remove_slackbot_msgs.py
+++ remove_slackbot_msgs.py
@@ -9,19 +9,20 @@ s = SlackCleaner(SLACK_TOKEN, sleep_for=1)
 deleted = 0
 retained = 0
 first_lines = defaultdict(int)
+chars = 100
 for msg in s.c.slackbot.msgs(after="20200802"):
-    if re.match("push_day_old_data.py", msg.text) is not None:
+    if re.match("push_day_old_data.py", msg.text) is not None: # or re.match("fetch_terminals.py: No 'Location' field found for terminal with ID 55141", msg.text) is not None:
         time.sleep(1)
         msg.delete()
         deleted += 1
     else:
-        first_line = msg.text.split('\n')[0][:100]
+        first_line = msg.text.split('\n')[0][:chars]
         first_lines[first_line] += 1
         print(msg.text)
         print("="*80)
         retained += 1
 
-for f,c in first_lines.items():
-    print(c,f)
-    print("-"*80)
+for f,count in sorted(first_lines.items(), key=lambda item: item[1]):
+    print(f"{count:>5}: {f}")
+    print("-"*(chars+7))
 print(f"Deleted {deleted} Slackbot messages and kept {retained} messages.")
